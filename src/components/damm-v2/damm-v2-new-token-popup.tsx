edb039c1fc60c85899fcb3616c8124c35347746b
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2025-09-21 06:19:12 +0800

    Change Top Token to New Token

--- src/components/damm-v2/damm-v2-new-token-popup.tsx
+++ src/components/damm-v2/damm-v2-new-token-popup.tsx
@@ -1,31 +1,26 @@
 // src/components/damm-v2/damm-v2-new-token-popup.tsx
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { TokenData, TokenCard } from './damm-v2-token-card';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { TokenData, TokenCard } from './damm-v2-token-card'
 
 interface NewTokenPopupProps {
-  token: TokenData | null;
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
+  token: TokenData | null
+  open: boolean
+  onOpenChange: (open: boolean) => void
 }
 
 export function NewTokenPopup({ token, open, onOpenChange }: NewTokenPopupProps) {
   if (!token) {
-    return null;
+    return null
   }
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="bg-[#2a2a3e] border-0 text-white">
         <DialogHeader>
-          <DialogTitle>Top Token Changed! 😈</DialogTitle>
+          <DialogTitle>New Signal!</DialogTitle>
         </DialogHeader>
         <TokenCard token={token} />
       </DialogContent>
     </Dialog>
-  );
+  )
 }
